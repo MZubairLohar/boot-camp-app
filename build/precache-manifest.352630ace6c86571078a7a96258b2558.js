a0c5559785c79757bc216620f570f95d2846a42e
MZubairLohar <zubairgituser@gmail.com>
2020-06-27 17:08:13 +0500

    Initial commit

---
+++ build/precache-manifest.352630ace6c86571078a7a96258b2558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eea5ccb48ffc912295f24538b8f3468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6476cd981dfd0f5fc821",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7aec97bac354d219f5c8",
+    "url": "/static/js/2.ebb67645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebb67645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6476cd981dfd0f5fc821",
+    "url": "/static/js/main.3d701a50.chunk.js"
+  },
+  {
+    "revision": "4a422a9b5f7f9ad2c3d4",
+    "url": "/static/js/runtime-main.582389d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
